f5356f5b395e9e76795f8d056294eda11ea45697
Humberto Ramírez <humberto.rjdh@gmail.com>
2020-02-05 10:07:02 -0600

    se agrega archivo de funciones con strings

---
+++ string_fn.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def randomStringDigits(stringLength=6):
+    lettersAndDigits =string.ascii_letters + string.digits
+    return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+
+def randomDigits(strLength=8):
+    digits=string.digits
+    return ''.join(random.choice(digits) for i in range(strLength))
